57f681e891a38a6f4f0465a375c91aeeeee00219
Kaspar Aasa <kasparaasa@gmail.com>
2023-06-20 13:47:55 +0200

    Add blog interface

---
+++ src/interfaces/BlogPost.tsx
@@ -0,0 +1,7 @@
+export interface BlogPostInterface {
+  title: string
+  content: string
+  id: string
+  slug: string
+  date: string
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,10 @@ import React, {useState} from 'react'
 
 export default function Project() {
 
+
   return (
     <Layout>
-
+      <h1 className={'text-6xl font-bold text-slate-900'}>Hello World</h1>
     </Layout>
   )
 }
