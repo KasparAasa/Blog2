596e3a62157dfd72776d6ca3de23c53c75042166
Kaspar <kasparaasa@gmail.com>
2023-06-21 16:18:49 +0200

    Install uuid

--- src/pages/addPost.tsx
+++ src/pages/addPost.tsx
@@ -3,6 +3,7 @@ import {useState} from 'react'
 import {BlogPostInterface} from '@/interfaces/BlogPost'
 import * as Yup from 'yup'
 import {Formik, Field, Form, ErrorMessage} from 'formik'
+import {kebabCase} from "tiny-case";
 
 // const validate = (values: any) => { // function validate(values: any) {}
 //   const errors: any = {}
@@ -17,54 +18,52 @@ import {Formik, Field, Form, ErrorMessage} from 'formik'
 //   }
 // }
 
-export default function AddPost() {
 
-  const [blogPosts, setBlogPosts] =
-    useState<BlogPostInterface>({
-      title: '',
-      content: '',
-      id: '',
-      slug: '',
-      date: '',
-    })
+export default function AddPost() {
+  const [blogPosts, setBlogPosts] = useState<BlogPostInterface[]>([])
 
   return (
     <Layout>
       <div className={''}>
-      <Formik
-        initialValues={{
-          title: '',
-          content: '',
-          id: '',
-          slug: '',
-          date: '',
-        }}
-        validationSchema={Yup.object({
-          title: Yup.string()
-            .max(20, 'Must be 20 characters or less')
-            .required(`Title is required`),
-          content: Yup.string()
-            .required('Content is required'),
-        })}
-        onSubmit={(values) => {
-          setBlogPosts({
-            title: values.title,
-            content: values.content,
+        <Formik
+          initialValues={{
+            title: '',
+            content: '',
             id: '',
             slug: '',
             date: '',
-          })
-        }}
-      >
+          }}
+          validationSchema={Yup.object({
+            title: Yup.string()
+              .max(20, 'Must be 20 characters or less')
+              .required(`Title is required`),
+            content: Yup.string()
+              .required('Content is required'),
+          })}
+          onSubmit={(values) => {
+
+            const newList = [
+              ...blogPosts,
+              {
+                title: values.title,
+                content: values.content,
+                id: '',
+                slug: kebabCase(values.title),
+                date:'',
+              }
+            ]
+            setBlogPosts(newList)
+          }}
+        >
           <Form className={'flex flex-col gap-3'}>
             <label htmlFor={'title'}>Title</label>
             <Field
-              className={'border-1 rounded shadow border-slate-400 mb-5'}
+              className={'border-1 rounded shadow border-slate-400'}
               name={'title'}
               type={'text'}/>
             <ErrorMessage name={'title'}/>
 
-            <label htmlFor={'content'}>Content</label>
+            <label htmlFor={'content'} className={'mt-5'}>Content</label>
             <Field
               className={'border-1 border-slate-400 rounded shadow h-28'}
               name={'content'}
@@ -73,7 +72,15 @@ export default function AddPost() {
 
             <button type={'submit'}>Submit</button>
           </Form>
-      </Formik>
+        </Formik>
+      </div>
+      <div>
+        {blogPosts.map(post =>
+          <div>
+            <div>{post.title}</div>
+            <div>{post.content}</div>
+          </div>
+        )}
       </div>
     </Layout>
   )
